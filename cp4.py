f193bd0afb4512f150d38d8a3a332f3d6f7f2221
sofiapetruk <sofiapetruk900@gmail.com>
2024-08-26 08:46:05 -0300

    inicialização dos def

---
+++ cp4.py
@@ -0,0 +1,91 @@
+def incializarTabuleiro(n_linhas, n_colunas):
+    
+
+
+
+
+
+
+def imprimirTabuleiro()
+
+
+
+
+
+
+def imprimirMenuPrincipal()
+
+
+
+
+
+
+def leiaCoordenadaLinha()
+
+
+
+
+
+
+def imprimePontuacao()
+
+
+
+
+
+
+def posicaoValida()
+
+
+
+
+
+def verificaVencedor()
+
+
+
+
+
+def verificaVelha()
+
+
+
+
+
+
+def modoJogador()
+
+
+
+
+
+
+def modoFacil()
+
+
+
+
+
+
+def modoDificil()
+
+
+
+
+
+
+def jogadaUsuario()
+
+
+
+
+
+
+
+def jogadaMaquinaFacil()
+
+
+
+
+
+def jogadaMaquinaDificil()
