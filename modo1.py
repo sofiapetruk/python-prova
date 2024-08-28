e1ea4d6d15b8e1bcbd0a812649fd31c69bf7432e
sofiapetruk <sofiapetruk900@gmail.com>
2024-08-27 21:00:51 -0300

    modo1

--- modo1.py
+++ modo1.py
@@ -1,24 +1,34 @@
 def incializarTabuleiro():
-    matriz = [[" ", " ", " "],
-              [" ", " ", " "],
-              [" ", " ", " "]]
+    matriz = [
+        ["1", "2", "3"],
+        ["4", "5", "6"],
+        ["7", "8", "9"]
+        ]
     
     return matriz
 
-def imprimirTabuleiro(matriz):
-    """for linha in range(len(matriz)):
-        for coluna in range(len(matriz[linha])):
-            print(f"[{linha}][{coluna}]")"""
+def imprimirTabuleiro(matriz): 
+    for linha in range(len(matriz)):
+        for coluna in range(len(matriz)):
+            print(matriz[0])
+            print(matriz[1])
+            print(matriz[2])
+
+
+def escolhaJogador(matriz):
+    jogador1 = int(input('Escolha um número para colocar o X: '))
+    matriz[jogador1-1] = "X"
     print(matriz)
-  
+    jogador2 = int(input('Escolha um número para colocar o O'))
+    matriz[jogador2-1] = "O"
+    return jogador1, jogador2
+    
 
 
-
-#https://dev.to/iugstav/python-manipulacao-de-listas-e-matrizes-4bpj
-#https://awari.com.br/como-criar-e-manipular-matrizes-em-python-guia-completo-para-iniciantes/
 
 #Principal
 matriz = incializarTabuleiro()
 imprimirTabuleiro(matriz)
+jogador1, jogador2 = escolhaJogador(matriz)
 
 
